418bbc79cc71858190baea3e808ebb0ec2df9ebe
Thanawan Chaemsathian <74282946+Scorpionzaa555@users.noreply.github.com>
2024-07-18 14:32:05 +0700

    Update app.py

--- 3_fastapi_with_postgresql/app.py
+++ 3_fastapi_with_postgresql/app.py
@@ -57,28 +57,41 @@ async def create_book(book: dict, response: Response, db: Session = Depends(get_
 # async def delete_book(book_id: int, db: Session = Depends(get_db)):
 #     pass
 
-# Show student info
+@router_v1.patch('/books/{book_id}')
+async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
+    db_item = db.query(models.Book).filter(models.Book.id == book_id).first()
+    for key, value in book.items():
+            setattr(db_item, key, value)
+    db.commit()
+    db.refresh(db_item)
+    # response.status_code = 201
+    return db_item
+
+@router_v1.delete('/books/{book_id}')
+async def delete_book(book_id: int,db: Session = Depends(get_db)):
+    db_item = db.query(models.Book).filter(models.Book.id == book_id).first()
+    db.delete(db_item)
+    db.commit()
+    return "Delete successfully !!!"
+
 @router_v1.get('/students')
 async def get_students(db: Session = Depends(get_db)):
     return db.query(models.Student).all()
 
-# Show Student info follow id
 @router_v1.get('/students/{student_id}')
 async def get_students(id: int, db: Session = Depends(get_db)):
     return db.query(models.Student).filter(models.Student.id == id).first()
 
-# create new student
 @router_v1.post('/students')
 async def create_student(student: dict, response: Response, db: Session = Depends(get_db)):
     # TODO: Add validation
-    newstudent = models.Student(id=student['id'], name=student['name'], surname=student['surname'], bod=student['bod'], sex=student['sex'], age=student['age'])
+    newstudent = models.Student(name=student['name'], surname=student['surname'], bod=student['bod'], sex=student['sex'], age=student['age'])
     db.add(newstudent)
     db.commit()
     db.refresh(newstudent)
     response.status_code = 201
     return newstudent
 
-# update student
 @router_v1.patch('/students/{student_id}')
 # app.include_router(router_v1)
 async def update_student(id: int, student: dict, db: Session = Depends(get_db)):
@@ -87,15 +100,15 @@ async def update_student(id: int, student: dict, db: Session = Depends(get_db)):
             setattr(db_item, key, value)
     db.commit()
     db.refresh(db_item)
+    # response.status_code = 201
     return db_item
 
-# delete student
 @router_v1.delete('/students/{student_id}')
 async def delete_student(id: int, db: Session = Depends(get_db)):
     db_item = db.query(models.Student).filter(models.Student.id == id).first()
     db.delete(db_item)
     db.commit()
-    return "Delete successfully!!! Woo!!!"
+    return "Delete successfully!!!"
 
 app.include_router(router_v1)
 
